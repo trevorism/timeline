686d89a083401267fd44fbeb72130cdba97da0c0
Trevor Brooks <trevorism@gmail.com>
2023-12-25 09:20:44 -0500

    Add mixpanel to timeline app

--- src/app/src/main.js
+++ src/app/src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import VueClickAway from 'vue3-click-away'
 import { createVuestic } from 'vuestic-ui'
 import config from '../vuestic.config.js'
+import VueMixpanel from 'vue-mixpanel'
 import './style.css'
 
 const app = createApp(App)
@@ -12,4 +13,12 @@ const app = createApp(App)
 app.use(router)
 app.use(VueClickAway)
 app.use(createVuestic({ config }))
+app.use(VueMixpanel, {
+    token: "3ad96fc246692627d5addd73aa6072ae",
+    config: {
+        track_pageview: true,
+        secure_cookie: true,
+        same_site: 'None'
+    },
+})
 app.mount('#app')
