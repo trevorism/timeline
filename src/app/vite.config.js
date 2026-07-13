de7fdec84a32a22d421c46a563cabdefa07483d1
Trevor Brooks <trevorism@gmail.com>
2026-07-12 23:42:42 -0400

    timeline update. java 17 -> 25, micronaut 4 -> 5. Update js dependencies

--- src/app/vite.config.js
+++ src/app/vite.config.js
@@ -1,8 +1,9 @@
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [tailwindcss(), vue()],
     server: {
         host: "localhost",
         proxy: {
